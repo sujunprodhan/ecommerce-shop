563e32f1cfb9939aa67d548755ee89f57f798440
sujunprodhan <144489247+sujunprodhan@users.noreply.github.com>
2026-02-15 00:08:23 +0600

    navbar design

--- src/components/header/Hearder.jsx
+++ src/components/header/Hearder.jsx
@@ -1,11 +1,17 @@
+'use client'
 import React from 'react';
 import Logo from '../common/Logo';
 import Container from '../common/Container';
 import Link from 'next/link';
 import { XIcon } from 'lucide-react';
+import { navItem } from '@/constants/data';
+import { usePathname } from 'next/navigation';
 
 
 const Header = () => {
+  const pathname = usePathname()
+  const getSignInUrl = '/signin'
+  const signUpUrl = '/signup'
   return (
     <div>
       <header className="border-b p-4">
@@ -14,13 +20,40 @@ const Header = () => {
             <div>
               <Logo></Logo>
             </div>
-            <div className='flex gap-4'>
-              <Link href={'/'}>Home</Link>
-              <Link href={'/about'}>About</Link>
+            <div className="hidden md:inline-flex justify-center items-center gap-7 text-sm uppercase font-semibold text-light-color">
+              {navItem?.map((item) => (
+                <Link
+                  key={item?.title}
+                  href={item?.href}
+                  className={`hover:text-green-600 hoverEffect relative group ${
+                    pathname === item?.href ? 'text-green-600' : ''
+                  }`}
+                >
+                  {item.title}
+                  <span
+                    className={`absolute bottom-0 left-1/2 w-0 h-0.5 bg-green-600 group-hover:w-1/2 group-hover:left-0 hoverEffect ${pathname === item?.href && 'w-1/2'}`}
+                  />
+                  <span
+                    className={`absolute bottom-0 right-1/2 w-0 h-0.5 bg-green-600 group-hover:right-0 group-hover:w-1/2 hoverEffect ${pathname === item?.href && 'w-1/2'}`}
+                  />
+                </Link>
+              ))}
             </div>
-            <div>
-              <XIcon></XIcon>
+            <div className='flex gap-2 items-center'>
+              <Link
+                href={getSignInUrl}
+                className="bg-transparent border border-green-600 hover:bg-green-700 hover:text-white px-2 py-1.5 rounded-md font-semibold hoverEffect duration-300"
+              >
+                Sign in
+              </Link>
+              <Link
+                href={signUpUrl}
+                className="bg-green-700  border text-white border-green-600 hover:bg-white hover:text-black px-2 py-1.5 rounded-md font-semibold hoverEffect duration-300"
+              >
+                Sign Up
+              </Link>
             </div>
+            <div></div>
           </div>
         </Container>
       </header>
---
+++ src/constants/data.js
@@ -0,0 +1,25 @@
+const { isHmrRefresh } = require('next/dist/server/app-render/work-unit-async-storage.external');
+
+const navItem = [
+  {
+    title: 'Home',
+    href: '/',
+  },
+  {
+    title: 'Shop',
+    href: '/shop',
+  },
+  {
+    title: 'HotDeal',
+    href: '/hotdeal',
+  },
+  {
+    title: 'Blog',
+    href: '/blog',
+  },
+  {
+    title: 'Contact',
+    href: '/contact',
+  },
+];
+export {navItem}
